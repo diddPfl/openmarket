415f9ca1a41b22cf7fef25f776a59e919a93e8db
Lee.Eeea <leegaeun0805@gmail.com>
2024-08-22 18:44:05 +0900

    header, cart, payment.

--- src/main/java/com/javalab/board/config/SecurityConfig.java
+++ src/main/java/com/javalab/board/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.javalab.board.config;
 
 import com.javalab.board.handler.AuthFailureHandler;
-import com.javalab.board.handler.AuthSucessHandler;
 import com.javalab.board.security.MemberService;
+import com.javalab.board.security.handler.CustomSocialLoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,9 +23,9 @@ public class SecurityConfig {
 
     private final MemberService memberService;
     private final AuthFailureHandler authFailureHandler;
-    private final AuthenticationSuccessHandler customSocialLoginSuccessHandler;
     private final AccessDeniedHandler accessDeniedHandler;
     private final PasswordEncoder passwordEncoder;
+    private final CustomSocialLoginSuccessHandler customSocialLoginSuccessHandler;
 
     @Autowired
     public SecurityConfig(
@@ -32,9 +33,7 @@ public class SecurityConfig {
             AuthFailureHandler authFailureHandler,
             AccessDeniedHandler accessDeniedHandler,
             PasswordEncoder passwordEncoder,
-            @Qualifier("customSocialLoginSuccessHandler")
-            AuthenticationSuccessHandler customSocialLoginSuccessHandler) {
-
+            CustomSocialLoginSuccessHandler customSocialLoginSuccessHandler) {
         this.memberService = memberService;
         this.authFailureHandler = authFailureHandler;
         this.accessDeniedHandler = accessDeniedHandler;
@@ -43,62 +42,67 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder);
+        return auth.build();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
         auth.userDetailsService(memberService).passwordEncoder(passwordEncoder);
 
         http
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/member/login")
-                        .loginProcessingUrl("/member/login")
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))  // Disable CSRF for API requests
+
+                /*.formLogin(formLogin -> formLogin
+                        .loginPage("/login")
                         .usernameParameter("email")
                         .passwordParameter("password")
-                        .successHandler(customSocialLoginSuccessHandler)
+                        .successHandler(customSocialLoginSuccessHandler)  // Use custom success handler
                         .failureHandler(authFailureHandler)
-                )
+                        .permitAll()
+                )*/
                 .logout(logout -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                        .logoutSuccessUrl("/member/login")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                         .permitAll()
                 )
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/mypage/cart", "/mypage/cart/**").permitAll()
-                        .requestMatchers("/mypage/**").permitAll()
-                        .requestMatchers("/mypage/reviews").permitAll()
-                        .requestMatchers("/order/**").permitAll()
+                        .requestMatchers("/api/members", "/api/login", "/api/logout", "/login", "/signup").permitAll()
                         .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/fonts/**", "/ckeditor2/**", "/vendor/**").permitAll()
                         .requestMatchers("/view/**", "/emp/**").permitAll()
-                        //.requestMatchers("/member/login", "/member/join/**", "/login").permitAll()
-                        .requestMatchers("/member/login", "/login").permitAll()
-                        .requestMatchers("/member/create").permitAll()  // Allow unauthenticated access to the member creation page
-                        .requestMatchers("/member/modify").hasRole("USER")
+                        .requestMatchers("/", "/{path:[^\\.]*}").permitAll()
                         .requestMatchers("/board/**").permitAll()
                         .requestMatchers("/item/view/**", "/item/list/**", "/item/read/**").permitAll()
-                        .requestMatchers("/mypage").permitAll()
+                        .requestMatchers("/api/mypage").authenticated()
+                        .requestMatchers("/mypage/**", "/mypage/cart/**", "/mypage/reviews", "/order/**").permitAll()
+                        .requestMatchers("/mypage/cart/payment/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/member/modify").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/item/register/**", "/item/modify/**", "/item/remove/**").hasRole("ADMIN")
-                        .requestMatchers("/cart/**", "cartItem/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/cart/**", "/cartItem/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/track/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
-                ) // 아이디 기억 관련 설정
+                )
                 .rememberMe(rememberMe -> rememberMe
-                        .userDetailsService(memberService)  // Set UserDetailsService for remember-me
+                        .userDetailsService(memberService)
                         .alwaysRemember(false)
                         .tokenValiditySeconds(43200)
                         .rememberMeParameter("remember-me")
-                ) // 권한 없는 페이지 요청 처리
+                )
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .accessDeniedHandler(accessDeniedHandler)
                 )
                 .oauth2Login(oauth2Login -> oauth2Login
-                        .loginPage("/member/login")
-                        .successHandler(customSocialLoginSuccessHandler)
+                        .loginPage("/login")
+                        .successHandler(customSocialLoginSuccessHandler)  // Use custom success handler
                 );
 
         http.authenticationManager(auth.build());
         return http.build();
     }
-
 }
